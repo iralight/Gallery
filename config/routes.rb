3a6a277781d38487927b0982f2c86c89845f415d
Irina Likhtina <iralight@gmail.com>
2014-04-17 21:44:49 -0400

    make it a root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Example::Application.routes.draw do
   get "users/index"
+  root to: "users#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

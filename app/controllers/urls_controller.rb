7972883b1702ec02d2eee2e964cc8ae44390310e
Irina Likhtina <iralight@gmail.com>
2014-04-22 20:19:34 -0400

    latest changes

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,8 +1,20 @@
+ require 'net/http'
+ require 'nokogiri'
+ require 'open-uri'
+
 class UrlsController < ApplicationController
   def gallery
-  	require 'net/http'
-  	uri = URI('http://www.pinterest.com/iralight83/accessories/')
+
+  	sample_pinboard = 'http://www.pinterest.com/iralight83/accessories/'
+  	# get the url we are interested in
+  	uri = URI(sample_pinboard)
   	@result = Net::HTTP.get(uri)
-  	puts @result
+  	#puts @result
+
+  	# find <div class="hoverMask"> that contains the pins
+  	# Get a Nokogiri::HTML::Document for the page we’re interested in...
+	@doc = Nokogiri::HTML(@result)
+	
+
   end
 end

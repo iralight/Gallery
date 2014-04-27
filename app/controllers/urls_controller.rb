4be5c154604b49dff727817934fcebc7ffbd7be5
Irina Likhtina <iralight@gmail.com>
2014-04-27 17:30:33 -0400

    able to show the links but cannot grab them through css

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -9,13 +9,26 @@ class UrlsController < ApplicationController
   	# get the url we are interested in
   	uri = URI(sample_pinboard)
   	@result = Net::HTTP.get(uri)
-  	#puts @result
 
   	# Get a Nokogiri::HTML::Document for the page we’re interested in...
 	  doc = Nokogiri::HTML(@result)
 
-    #@links = doc.css('img[class="pinImg"]')
-    @links = doc.css('img[class^="pinImg"]')
+    # get only pins from the link
+    links = doc.css('img[class^="pinImg"]')
+
+    # filter out all the fluff surrounding the links
+    # and store results in array
+    @pins = []
+
+    links.each do |link| 
+      link.attributes.each do |attribute|
+        # first part of attribute is the name, 2nd is the Nokogori element
+        if attribute[0] = 'src'
+          @pins.push attribute[1]
+          #puts attribute[1].css('src')
+        end
+      end
+    end
 
   end
 end
